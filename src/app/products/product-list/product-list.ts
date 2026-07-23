6c134714784180212eadeecade795d73efe1a268
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-07-23 19:12:03 +0530

    feat: add product images, data, and service for product listing

--- src/app/products/product-list/product-list.ts
+++ src/app/products/product-list/product-list.ts
@@ -1,19 +1,22 @@
 import { CommonModule, CurrencyPipe, LowerCasePipe } from '@angular/common';
-import { Component, computed, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../product';
 import { ConvertToSpacesPipe } from '../../shared/convert-to-spaces-pipe';
 import { Star } from '../../shared/star/star';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
   imports: [CommonModule, FormsModule, LowerCasePipe, CurrencyPipe, ConvertToSpacesPipe, Star],
   templateUrl: './product-list.html',
-  styles: [`
-    thead {
-      color: #337AB7
-    }
-    `]
+  styles: [
+    `
+      thead {
+        color: #337ab7;
+      }
+    `,
+  ],
 })
 export class ProductList implements OnInit {
   pageTitle = signal('Product List');
@@ -21,18 +24,9 @@ export class ProductList implements OnInit {
   showImage = signal(false);
   listFilter = signal('');
 
-  products = signal<Product[]>([
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-  ]);
+  private productService = inject(ProductService);
+
+  readonly products = signal<ReadonlyArray<Product>>(this.productService.getProducts());
 
   filteredProducts = computed(() => {
     const filter = this.listFilter().toLowerCase().trim();
@@ -50,6 +44,6 @@ export class ProductList implements OnInit {
   }
 
   onRatingClicked(message: string) {
-    this.pageTitle.set(`Product List ${message}` )    
+    this.pageTitle.set(`Product List ${message}`);
   }
 }
