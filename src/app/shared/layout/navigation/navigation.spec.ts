13dcf431b7fbe94e14d9d637411c4c41d56dba63
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-07-25 13:50:30 +0530

    test: add unit tests for App component and Navigation component

--- src/app/shared/layout/navigation/navigation.spec.ts
+++ src/app/shared/layout/navigation/navigation.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Navigation } from './navigation';
+import { provideRouter } from '@angular/router';
 
 describe('Navigation', () => {
   let component: Navigation;
@@ -9,14 +10,23 @@ describe('Navigation', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Navigation],
+    providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Navigation);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('title', 'apm-22');
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the application title', () => {
+    // Assert
+    const heading = fixture.nativeElement.querySelector('h1');
+    expect(heading?.textContent).toContain('apm-22');
   });
 });
